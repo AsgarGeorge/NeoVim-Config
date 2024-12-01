6e120edd215344cfd88e57f3dc0a3a8e8dc5fb50
Asgar_George <asgarantony@gmail.com>
2024-12-01 16:55:20 +0530

    adding tree sitter

--- init.lua
+++ init.lua
@@ -25,7 +25,8 @@ vim.opt.rtp:prepend(lazypath)
 local opts = {}
 local plugins = {
   {"folke/tokyonight.nvim",lazy = false,priority = 1000},
-  {'nvim-telescope/telescope.nvim', tag = '0.1.8',dependencies = { 'nvim-lua/plenary.nvim' }}
+  {'nvim-telescope/telescope.nvim', tag = '0.1.8',dependencies = { 'nvim-lua/plenary.nvim' }},
+  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"}
 
 }
 require("lazy").setup(plugins, opts)
